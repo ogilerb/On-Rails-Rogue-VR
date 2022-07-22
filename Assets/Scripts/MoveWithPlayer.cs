55a22343487846204432b9b0c69ee7c84b539890
MaxG <maxgarescarra@gmail.com>
2022-07-22 09:16:49 -0700

    a

--- Assets/Scripts/MoveWithPlayer.cs
+++ Assets/Scripts/MoveWithPlayer.cs
@@ -17,7 +17,7 @@ public class MoveWithPlayer : MonoBehaviour
         foreach (GameObject obj in environment)
         {
             currentPos = obj.transform.position;
-            currentPos.z += 0.1f;
+            currentPos.z += 5f * Time.deltaTime;
             obj.transform.position = currentPos;
         }
 
